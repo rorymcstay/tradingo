968fcc201284d8c3549de9fcec27e6369a4cac2b
Rory McStay <rory@rorymcstay.com>
2021-06-14 17:35:25 +0100

    Make buildable

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <model/Execution.h>
+
+using qty_t = long;
+using price_t = double;
+
+
+class MarketData
+{
+
+
+};
+
+class Strategy
+{
+
+};
+
+
+int main() {
+
+    auto exec = io::swagger::client::model::Execution();
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
