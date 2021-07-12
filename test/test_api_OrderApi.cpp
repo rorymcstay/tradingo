f04bd4b58f0c39ed30089272cc2d39d8cd4fb452
Rory McStay <rory@rorymcstay.com>
2021-07-12 17:21:12 +0100

    test order api for throwing exception

--- test/test_api_OrderApi.cpp
+++ test/test_api_OrderApi.cpp
@@ -1,33 +1,56 @@
-//
-// Created by rory on 03/07/2021.
-//
 #include <gtest/gtest.h>
-#include <Config.h>
-#include <MarketData.h>
+#include <chrono>
+
+// src
+#include "Config.h"
+#include "MarketData.h"
+
+// CppRestSwaggerClient
 #include "api/OrderApi.h"
 
 using namespace io::swagger::client;
 
-TEST(OrderApi, DISABLED_order_newBulk) {
+struct OrderManager {
+    std::shared_ptr<api::OrderApi> orderApi;
+    long oidSeed;
+    OrderManager()
+    :   orderApi(nullptr)
+    ,   oidSeed(std::chrono::system_clock::now().time_since_epoch().count()){
 
-    auto config = std::make_shared<Config>();
-    config->set("apiKey", "-rqipjFxM43WSRKdC8keq83K");
-    config->set("apiSecret", "uaCYIiwpwpXNKuVGCBPWE3ThzvyhOzKs6F9mWFzc9LueG3yd");
-    config->set("symbol", "XBTUSD");
-    config->set("baseUrl", "https://testnet.bitmex.com/api/v1/");
-    config->set("connectionString", "wss://testnet.bitmex.com");
-    std::string base_url = config->get("baseUrl");
-    auto apiConfig = std::make_shared<api::ApiConfiguration>();
-    auto httpConfig = web::http::client::http_client_config();
-    apiConfig->setHttpConfig(httpConfig);
-    apiConfig->setBaseUrl(base_url);
-    apiConfig->setApiKey("api-key", config->get("apiKey"));
-    apiConfig->setApiKey("api-secret", config->get("apiSecret"));
-    auto apiClient = std::make_shared<api::ApiClient>(apiConfig);
-    auto orderManager = std::make_shared<api::OrderApi>(apiClient);
+        auto config = std::make_shared<Config>();
+        config->set("apiKey", "-rqipjFxM43WSRKdC8keq83K");
+        config->set("apiSecret", "uaCYIiwpwpXNKuVGCBPWE3ThzvyhOzKs6F9mWFzc9LueG3yd");
+        config->set("symbol", "XBTUSD");
+        config->set("baseUrl", "https://testnet.bitmex.com/api/v1/");
+        config->set("connectionString", "wss://testnet.bitmex.com");
+        std::string base_url = config->get("baseUrl");
+        auto apiConfig = std::make_shared<api::ApiConfiguration>();
+        auto httpConfig = web::http::client::http_client_config();
+        apiConfig->setHttpConfig(httpConfig);
+        apiConfig->setBaseUrl(base_url);
+        apiConfig->setApiKey("api-key", config->get("apiKey"));
+        apiConfig->setApiKey("api-secret", config->get("apiSecret"));
+        auto apiClient = std::make_shared<api::ApiClient>(apiConfig);
+        orderApi = std::make_shared<api::OrderApi>(apiClient);
+    }
+
+    std::string to_string(std::initializer_list<model::Order> orders_) {
+        auto lis = web::json::value::array();
+        int count = 0;
+        for (auto& order : orders_) {
+            lis[count++] = order.toJson();
+        }
+        return lis.serialize();
+    }
+
+};
+
+TEST(OrderApi, order_newBulk) {
+
+    OrderManager om {};
 
     std::string orders = R"([{"clOrdID":"MCST0","orderID":"","orderQty":50,"price":33709.5,"side":"Buy","symbol":"XBTUSD"},{"clOrdID":"MCST1","orderID":"","orderQty":50,"price":33710,"side":"Sell","symbol":"XBTUSD"}])";
-    auto newOrders = orderManager->order_newBulk(orders).then(
+    auto newOrders = om.orderApi->order_newBulk(orders).then(
             [](const std::vector<std::shared_ptr<model::Order>> &orders_) {
                 for (const auto& order : orders_) {
                     std::cout << order->toJson().serialize() << '\n';
@@ -35,4 +58,39 @@ TEST(OrderApi, DISABLED_order_newBulk) {
             }
     );
     newOrders.get();
+}
+
+TEST(OrderApi, order_newBulk_throws_ApiException) {
+
+    OrderManager om {};
+    model::Order order1 {};
+    order1.setOrderQty(100);
+    order1.setPrice(33370.5);
+    order1.setSide("Buy");
+    order1.setClOrdID(std::to_string(om.oidSeed));
+    order1.setSymbol("XBTUSD");
+    model::Order order2 {};
+    order2.setOrderQty(100);
+    order2.setPrice(33370.5);
+    order2.setSide("Buy");
+    order2.setClOrdID(std::to_string(om.oidSeed));
+    order2.setSymbol("XBTUSD");
+
+    std::error_code res;
+    auto newOrders = om.orderApi->order_newBulk(om.to_string({order1, order2})).then(
+            [&res](const std::vector<std::shared_ptr<model::Order>> &orders_) {
+                try {
+                    for (const auto& order : orders_) {
+                        std::cout << order->toJson().serialize() << '\n';
+                    }
+                } catch (api::ApiException& ex_ ) {
+                    res = ex_.error_code();
+                    LOGINFO("ApiException: " << ex_.getContent()->rdbuf());
+                }
+            }
+    );
+
+    newOrders.get();
+
+    LOGINFO(LOG_VAR(res));
 }
