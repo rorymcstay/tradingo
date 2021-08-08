7771debeb6a1fe86bacfdc16234d1b5fa474de3c
Rory McStay <rory@rorymcstay.com>
2021-08-08 15:03:26 +0000

    install service

--- app/scripts/tickrecorder.sh
+++ app/scripts/tickrecorder.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+source "$(dirname ${BASH_SOURCE[0]})/profile.env"
+
 tick_record() {
-    tickRecorder --config /home/rory/tickRecorder.cfg
+    tickRecorder --config $INSTALL_LOCATION/etc/config/tickRecorder.cfg
     return $? 
 }
 tick_record
