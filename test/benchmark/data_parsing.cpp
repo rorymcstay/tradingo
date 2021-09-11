0b771cc85ed8aa432db10da30631ed988bc090c9
Rory McStay <rory@rorymcstay.com>
2021-09-11 01:33:03 +0000

    add missing vars

--- test/benchmark/data_parsing.cpp
+++ test/benchmark/data_parsing.cpp
@@ -20,16 +20,19 @@ static void BM_read_quotes_json(benchmark::State& state) {
 
     for (auto kvp : std::initializer_list<std::pair<std::string, std::string>>({
          {"symbol", "XBTUSD"},
+         {"storage", "/tmp/"},
          {"moving_average_crossover-callback", "false"},
          {"override-signal-callback", "true"},
-         {"moving_average_crossover-interval", "1000"}})
+         {"moving_average_crossover-interval", "1000"},
+         {"shortTermWindow", "1000"},
+         {"longTermWindow", "8000"}}
+         )
     ) config->set(kvp.first, kvp.second);
 
     auto marketdata = std::make_shared<TestMarketData>(config);
     auto signal = std::make_shared<MovingAverageCrossOver>(1000, 8000);
     signal->init(config, marketdata);
     marketdata->setCallback([&](){ signal->update(); });
-    state.
     for (auto _ : state) {
         auto quote = getEvent<model::Quote>(quoteFile);
         *marketdata << quote;
