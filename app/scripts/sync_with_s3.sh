62a03f3cf766606211d74e7673edb6391d676c22
Rory McStay <rory@rorymcstay.com>
2021-09-25 10:17:59 +0000

    sync with s3 install

---
+++ app/scripts/sync_with_s3.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p /home/rory/tickRecorder/log/
+aws s3 sync /data/tickRecorder/storage/ s3://tick-storage/tickRecorder/storage &> /home/rory/tickRecorder/log/sync_with_s3_`date +%Y%m%d`.log
