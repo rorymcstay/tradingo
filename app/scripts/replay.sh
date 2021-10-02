c1713fd2e1f7c6f0dbac4989e250f48cd0dd18c4
Rory McStay <rory@rorymcstay.com>
2021-10-02 16:34:22 +0000

    replay script working local

--- app/scripts/replay.sh
+++ app/scripts/replay.sh
@@ -3,7 +3,9 @@ source "$(dirname ${BASH_SOURCE[0]})/profile.env"
 
 replay_tradingo_on() {
 
-    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/2021-09-08/quotes_$SYMBOL.json" "$TICK_STORAGE"
+    mkdir -p "$TICK_STORAGE/$1"
+    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/$1/quotes_$SYMBOL.json" "$TICK_STORAGE/$1/"
+    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/$1/trades_$SYMBOL.json" "$TICK_STORAGE/$1/"
     run_date=$(date -I)
     eval "$(date +'today=%F now=%s')"
     midnight=$(date -d "$today 0" +%s)
@@ -16,13 +18,11 @@ replay_tradingo_on() {
     DATESTR=$1 \
     RUN_ID=$run_id \
     INSTALL_LOCATION=$INSTALL_LOCATION \
-    # parameterizable through env vars
-    MOVING_AVG_CALLBACK=${MOVING_AVG_CALLBACK:-false}
-    MOVING_AVG_INTERVAL=${MOVING_AVG_INTERVAL:-1000}
-    MOVING_AVG_SHORT_TERM=${MOVING_AVG_SHORT_TERM:-1000}
-    MOVING_AVG_LONG_TERM=${MOVING_AVG_LONG_TERM:-8000}
-    SYMBOL=${SYMBOL:-XBTUSD}
-
+    MOVING_AVG_CALLBACK=${MOVING_AVG_CALLBACK:-false} \
+    MOVING_AVG_INTERVAL=${MOVING_AVG_INTERVAL:-1000} \
+    MOVING_AVG_SHORT_TERM=${MOVING_AVG_SHORT_TERM:-1000} \
+    MOVING_AVG_LONG_TERM=${MOVING_AVG_LONG_TERM:-8000} \
+    SYMBOL=${SYMBOL:-XBTUSD} \
         envsubst < $INSTALL_LOCATION/etc/config/replayTradingo.cfg  \
     > /tmp/replay.cfg
     cat /tmp/replay.cfg
