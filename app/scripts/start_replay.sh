49d8f258e43716e980e2caaf9edb4dde31dc7c1e
Rory McStay <rory@rorymcstay.com>
2021-10-04 00:45:29 +0100

    improve datestring

--- app/scripts/start_replay.sh
+++ app/scripts/start_replay.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 source "$(dirname ${BASH_SOURCE[0]})/profile.env"
 
+trade_date=$1
+
 replay_tradingo_on() {
     # exit on first failure
     set -e
 
     # params
     DATESTR=$1
+    if [[ ! $DATESTR ]]; then
+        echo "Datestring must not be empty"
+        return 1
+    fi
 
-    mkdir -p "$TICK_STORAGE/$1"
-    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/$1/quotes_$SYMBOL.json" "$TICK_STORAGE/$1/"
-    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/$1/trades_$SYMBOL.json" "$TICK_STORAGE/$1/"
+    mkdir -p "$TICK_STORAGE/$DATESTR"
+    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/$1/quotes_$SYMBOL.json" "$TICK_STORAGE/$DATESTR/"
+    aws s3 cp "s3://$BUCKET_NAME/tickRecorder/storage/$1/trades_$SYMBOL.json" "$TICK_STORAGE/$DATESTR/"
     run_date=$(date -I)
     eval "$(date +'today=%F now=%s')"
     midnight=$(date -d "$today 0" +%s)
@@ -30,11 +36,10 @@ replay_tradingo_on() {
         envsubst < $INSTALL_LOCATION/etc/config/replayTradingo.cfg  \
     > $config_file
 
-
     populate_strategy_params $INSTALL_LOCATION/etc/config/strategy/${STRATEGY}.cfg >> $config_file
     cat $config_file
     # run the replay
     replayTradingo --config $config_file
     aws s3 sync "$REPLAY_STORAGE" "s3://$BUCKET_NAME/replays/"
 }
-replay_tradingo_on $1
+replay_tradingo_on $trade_date
