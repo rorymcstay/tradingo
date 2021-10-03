158b18dee70afda7cb162f619587f058532f2c9c
Rory McStay <rory@rorymcstay.com>
2021-10-03 13:58:18 +0100

    tidy up dockerfiles

--- app/scripts/build_tag_upload.sh
+++ app/scripts/build_tag_upload.sh
@@ -7,7 +7,7 @@ version=$(get_version $1)
 
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 
-docker build -f docker/tradingo$1.dockerfile --tag tradingo/$1:$version --tag tradingo/$1:$version .
+docker build -f docker/tradingo-$1.dockerfile --tag tradingo/$1:$version --tag tradingo/$1:$version .
 
 docker tag tradingo/$1:$version rmcstay95/tradingo-$1:$version
 
