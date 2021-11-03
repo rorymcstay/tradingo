29d7c0d6e95b75e00412f9543241377350e2098b
Rory McStay <rory@rorymcstay.com>
2021-11-03 00:43:38 +0000

    fix autogen script

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -r build
-rm compile_commands.json
 
 BUILD_TYPE=${BUILD_TYPE:-Release}
 REPLAY_MODE=${REPLAY_MODE:-0}
 
 BUILD_DIR=$(if [[ $BUILD_TYPE == 'Release' ]]; then echo build.release; else echo build.debug; fi)
+
+if [[ -d $BUILD_DIR ]] ; then
+    rm -r $BUILD_DIR
+fi
 
 mkdir $BUILD_DIR 
 
@@ -14,14 +16,10 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 
 cmake -DCPPREST_ROOT=/usr/ \
-    -DCMAKE_COMPILE_COMMANDS=1 \
-    -DCPPREST_ROOT=${CPPREST_ROOT:-/usr/}
-    "@" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCPPREST_ROOT=${CPPREST_ROOT:-/usr/} \
+    "$@" \
     ../
 )
 
-ln -s build/compile_commands.json compile_commands.json
-
-(
-cd $BUILD_DIR && make install -j6
-)
+ln -s $BUILD_DIR/compile_commands.json compile_commands.json
