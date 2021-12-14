a7f1453d5e4855c3b9e6345bc30fbe8513253a63
Rory McStay <rory@rorymcstay.com>
2021-12-14 14:49:20 +0000

    small fix ups

--- app/scripts/sync_with_s3.sh
+++ app/scripts/sync_with_s3.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 mkdir -p /home/rory/tickRecorder/log/
 aws s3 sync /data/tickRecorder/storage/ s3://tick-storage/tickRecorder/storage &> /home/rory/tickRecorder/log/sync_with_s3_`date +%Y%m%d`.log
--- test/test_api_PositionApi.cpp
+++ test/test_api_PositionApi.cpp
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
+
 #include "fwk/ApiManager.h"
+#include <cpprest/json.h>
 
 TEST(TestPositionApi, test_update_leverage) {
 
     ApiManager apiMgr{};
     apiMgr.positionApi->position_updateLeverage("XBTUSD", 10.0);
+
+}
+
+
+TEST(TestPositionApi, parse_json_float) {
+    
+    std::cout << web::json::value::parse("10.0000").serialize() << '\n';
 
 }
