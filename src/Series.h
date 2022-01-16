578bb11affd12a02fb09695ef0e37bde4d025d47
Rory McStay <rory@rorymcstay.com>
2022-01-16 17:07:31 +0000

    series indexing

--- src/Series.h
+++ src/Series.h
@@ -18,21 +18,73 @@ class Series {
 
     std::vector<std::shared_ptr<T>> _data;
     std::vector<long> _index;
-    utility::datetime::interval_type _start;
-    utility::datetime::interval_type _end;
+    utility::datetime::interval_type _start_interval;
+    utility::datetime::interval_type _end_interval;
+    utility::datetime _start_time;
+    utility::datetime _end_time;
     double _resolution;
     long _size;
 public:
+
+    class iterator_type {
+
+        std::vector<long>::const_iterator _index_iter;
+        const Series<T>* _series;
+
+        struct proxy {
+            const iterator_type _client;
+            T* operator->() const { 
+                auto& data = _client._series->_data;
+                return data[*(_client._index_iter)].get();
+            }
+        };
+    public:
+        iterator_type(
+                std::vector<long>::const_iterator index_,
+                const Series<T>* series_
+        )
+        :   _index_iter(index_)
+        ,   _series(series_) {}
+
+        iterator_type(const iterator_type& iterator_type_) {
+            this->_index_iter =  iterator_type_._index_iter;
+            this->_series = iterator_type_._series;
+        }
+        iterator_type operator++() { _index_iter++; return *this; }
+        bool operator!=(const iterator_type & other) const { 
+            return _series->_data[*_index_iter]->getTimestamp() != other->getTimestamp(); 
+        }
+        const T& operator*() const { return *_series->_data[*_index_iter]; }
+        const proxy operator->() const { 
+            return proxy { *this };
+        }
+
+    };
+    friend iterator_type;
+
+
     explicit Series(
             const std::string& dataFile_,
             int resolution_=1);
 
-    int size() const;
-
+    long size() const;
     std::shared_ptr<T> operator[] (const utility::datetime& time_) const;
-
     std::shared_ptr<T> operator[] (const std::string& time_) const;
+
+    iterator_type begin() const;
+    iterator_type end() const;
+    iterator_type operator++();
+    bool operator!=(const iterator_type& other) const;
+
+    void set_start(const std::string& start_);
+    void set_end(const std::string& end_);
+
+private:
+    long index_of(const utility::datetime& date_) const;
+
 };
+
+
 template <typename T>
 Series<T>::Series(const std::string& dataFile_, int resolution_) {
     std::ifstream dataFile;
@@ -54,43 +106,91 @@ Series<T>::Series(const std::string& dataFile_, int resolution_) {
     }
     std::sort(tmpdata.begin(), tmpdata.end(),
               [](const std::shared_ptr<T>& it1, const std::shared_ptr<T>& it2) {
-                return it1->getTimestamp() < it2->getTimestamp();
+                return it1->getTimestamp().to_interval() < it2->getTimestamp().to_interval();
               }
     );
-    _start = tmpdata[0]->getTimestamp().to_interval();
-    _end = tmpdata.back()->getTimestamp().to_interval();
-    auto size = _end - _start;
+    _start_interval = tmpdata[0]->getTimestamp().to_interval();
+    _start_time = tmpdata[0]->getTimestamp();
+    _end_interval = tmpdata.back()->getTimestamp().to_interval();
+    _end_time = tmpdata.back()->getTimestamp();
+    auto size = _end_interval - _start_interval + 1;
     _data = std::vector<std::shared_ptr<T>>(size, nullptr);
-    _size = tmpdata.size();
     for (auto item : tmpdata) {
-        long index = _end - item->getTimestamp().to_interval();
+        long index = item->getTimestamp().to_interval() - _start_interval;
         _data[index] = item;
         _index.push_back(index);
     }
 }
 
+
 template <typename T>
 std::shared_ptr<T> Series<T>::operator[](const utility::datetime& time_) const {
-    if (time_.to_interval() < _start) {
+    auto index = index_of(time_);
+    return _data[index];
+}
+
+template <typename T>
+long Series<T>::index_of(const utility::datetime& time_) const {
+    if (time_.to_interval() < _start_interval) {
         throw std::runtime_error("time out of bounds");
-    } else if (time_.to_interval() > _end) {
+    } else if (time_.to_interval() > _end_interval) {
         throw std::runtime_error("time out of bounds");
     }
-    auto index = _end - time_.to_interval();
-    for (int i(index); i--; i >= 0) {
+    auto index = std::min(time_.to_interval() - _start_interval, _data.size() - 1);
+    while (index >= 0) {
         if (_data.at(index))
-            return _data[index];
+            return index;
+        index--;
     }
     throw std::runtime_error("Series is empty");
 }
 
-template <typename T> int Series<T>::size() const { return _size; }
+
+template <typename T> long Series<T>::size() const { return _index.size(); }
+
 
 template <typename T>
 std::shared_ptr<T> Series<T>::operator[](const std::string& time_) const {
     auto time = utility::datetime::from_string(time_, utility::datetime::date_format::ISO_8601);
     return operator[](time);
 }
+
+template <typename T>
+void Series<T>::set_start(const std::string& start_) {
+    auto time = utility::datetime::from_string(start_, utility::datetime::date_format::ISO_8601);
+    _start_time = time;
+}
+
+template <typename T>
+void Series<T>::set_end(const std::string& end_) {
+    auto time = utility::datetime::from_string(end_, utility::datetime::date_format::ISO_8601);
+    _end_time = time;
+}
+
+
+template <typename T>
+typename Series<T>::iterator_type Series<T>::begin() const {
+    auto index = index_of(_start_time);
+    auto it = _index.begin();
+    while (*it <= index) {
+        it++;
+    }
+    typename Series<T>::iterator_type begin_ (it, this);
+    return begin_; 
+}
+
+
+template <typename T>
+typename Series<T>::iterator_type Series<T>::end() const {
+    auto index = index_of(_end_time);
+    auto it = _index.end();
+    while (*it >= index) {
+        it--;
+    }
+    typename Series<T>::iterator_type end_ (it, this);
+    return end_; 
+}
+
 
 template<typename T>
 std::shared_ptr<T> getEvent(std::ifstream &fileHandle_) {
--- test/TestSeries.cpp
+++ test/TestSeries.cpp
@@ -7,9 +7,13 @@
 #include <model/Quote.h>
 
 
-
 using namespace io::swagger::client;
 using namespace utility;
+
+datetime time_stamp(const std::string& timestamp_) {
+    return datetime::from_string(timestamp_, datetime::date_format::ISO_8601);
+}
+
 
 TEST(TestSeries, initialisation) {
 
@@ -18,7 +22,7 @@ TEST(TestSeries, initialisation) {
     auto series = Series<model::Quote>(quote_file);
     ASSERT_EQ(series.size(), 1000);
 
-    auto time_index = datetime::from_string("2021-10-05T00:00:19.678Z", datetime::date_format::ISO_8601);
+    auto time_index = time_stamp("2021-10-05T00:00:19.678Z");
     auto quote = std::make_shared<model::Quote>();
     auto quote_json = web::json::value::parse(R"({"askPrice":49244.5,"askSize":116600,"bidPrice":49244,"bidSize":1200,"symbol":"XBTUSD","timestamp":"2021-10-05T00:00:19.678Z"})");
     quote->fromJson(quote_json);
@@ -26,5 +30,17 @@ TEST(TestSeries, initialisation) {
 
     ASSERT_EQ(quote->getTimestamp().to_string(utility::datetime::date_format::ISO_8601),
         quote_retrieved->getTimestamp().to_string(utility::datetime::date_format::ISO_8601));
+
+    int count = 0;
+    for (auto& symbol : series) {
+        count++;
+        symbol.getSymbol();
+    }
+
+    ASSERT_EQ(count, 999);
+
+    ASSERT_EQ(series["2021-10-05T00:00:19.678Z"]->getAskPrice(), 49244.5);
+    ASSERT_EQ(series["2021-10-05T00:00:37.36Z"]->getAskPrice(), 49263.0);
+    ASSERT_EQ(series["2021-10-05T00:00:56.103Z"]->getAskPrice(), 49261.5);
 
 }
